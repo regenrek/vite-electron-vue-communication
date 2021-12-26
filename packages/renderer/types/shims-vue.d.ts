023d331ef3cf900fb34e0f458fac13de3c3f8166
regenrek <hello@regenrek.at>
2021-12-26 21:26:07 +0100

    up

--- packages/renderer/types/shims-vue.d.ts
+++ packages/renderer/types/shims-vue.d.ts
@@ -1,6 +1,9 @@
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
+  import type {DefineComponent} from 'vue';
   // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+
+
